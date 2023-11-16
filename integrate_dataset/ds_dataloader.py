e8a253015e673f4788e87f56c1d541aef3b3acea
Khalid <49135754+KhalidAlt@users.noreply.github.com>
2023-11-16 20:00:29 +0300

    Create ds_dataloader.py
    add testing script

---
+++ integrate_dataset/ds_dataloader.py
@@ -0,0 +1,64 @@
+"""This script test that your dataset has been convert into correct .json.gz format."""
+import gzip
+import json
+
+import datasets
+
+import glob 
+
+logger = datasets.logging.get_logger(name)
+
+
+_DESCRIPTION = """\
+"""
+
+_CITATION = """
+
+"""
+
+_Path = "/media/khalid/DATA/oscar/tmp/ar_oscar_*.json.gz"
+
+
+
+
+class OSCAR(datasets.GeneratorBasedBuilder):
+    """"""
+
+
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "text": datasets.Value("string"),
+                    'meta': datasets.Value("string"),
+
+                }
+            ),
+            supervised_keys=None,
+            homepage=_Path,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+
+        return [
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepaths": glob.glob(_Path)}),
+
+        ]
+
+    def _generate_examples(self, filepaths):
+        """This function returns the examples in the raw (text) form by iterating on all the files."""
+        id_ = 0
+        for filepath in filepaths:
+
+            logger.info("generating examples from = %s", filepath)
+            with gzip.open(open(filepath, "rb"), "rt", encoding="utf-8") as f:
+                for line in f:
+                    if line:
+                        example = json.loads(line)
+
+
+                        yield id_, {'text': example['text'], 'meta': example['meta']}
+                        id_ += 1
