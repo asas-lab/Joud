a897a512a076c59263eaa345a73b8cec7e77bf24
Khalid <49135754+KhalidAlt@users.noreply.github.com>
2023-11-17 23:33:46 +0300

    Create split_json.sh
    add script to split json files into small json files

---
+++ integrate_dataset/split_json.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 [NUMBER_OF_LINES] [SOURCE_FILE] [PREFIX]"
+    exit 1
+fi
+
+NUMBER_OF_LINES=$1
+SOURCE_FILE=$2
+PREFIX=$3
+
+# Split the file
+split -l $NUMBER_OF_LINES -d --additional-suffix=.json $SOURCE_FILE ${PREFIX}
+
+# Rename the first file (if it exists) to remove the leading zero
+if [ -f "${PREFIX}00.json" ]; then
+    mv "${PREFIX}00.json" "${PREFIX}0.json"
+fi
+
+# Rename the remaining files
+a=1
+for i in ${PREFIX}0*; do 
+  if [ -f "$i" ]; then
+    mv "$i" "${PREFIX}$a.jsonl"
+    let a=a+1
+  fi
+done
